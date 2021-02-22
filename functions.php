c77ded9e6d1b63cc602b9f92621dda0a030852cf
siamshaeed <siamshaeed@gmail.com>
2021-02-21 16:54:12 -0800

    21-02-2021

--- functions.php
+++ functions.php
@@ -1,31 +1,39 @@
 <?php 
 
 function zfinanbox_default_functions(){
+
 //Text Domain Load
 	load_theme_textdomain('zfinanbox', get_template_directory_uri().'/languages');
 
 //title ber dynamic
 	add_theme_support('title-tag');
+
 //post thumbnails add
 	add_theme_support('post_thumbnails');
 
 //Register Menu
-	register_nav_menus(array(
-		'mainmenu' => __('Mani Menu','zfinanbox'),
-		'secondaymenu' => __('Secondary Menu', 'zfinanbox')
-	));
-//Register for slider
-	register_post_type('zfinanbox',array(
-		'labels' => array(
-			'name' => 'My Sliders'
-		),
-		'public' => true,
-		'supports' => array('title', 'editor', 'thumbnails')
-	));
+register_nav_menus(array(
+	'mainmenu' => __('Mani Menu','zfinanbox'),
+	'secondaymenu' => __('Secondary Menu', 'zfinanbox')
+));
 
-	
+//Slider thumbnails add
+	add_theme_support( 'post-thumbnails' );
+	add_theme_support( 'post-thumbnails', array( 'post' ) );    // Posts only       
+	add_theme_support( 'post-thumbnails', array( 'page' ) );    // Pages only
+	add_theme_support( 'post-thumbnails', array( 'post', 'slider' ) ); // Posts and Movies
+//Register for slider
+register_post_type('zfinanbox',array(
+	'labels' => array(
+		'name' => 'My Sliders',
+		'add_new_item' =>'Add Your Slider Here'
+	),
+	'public' => true,
+	'supports' => array('title', 'editor', 'thumbnail', 'custom-fields')
+));
+
 }
 
-add_action('after_setup_theme','zfinanbox_default_functions');
 
+add_action('after_setup_theme','zfinanbox_default_functions');
 ?>
